1e59af5b06d37a57f9b0004d5205707dbfef9570
AnnaiaBokungu <108035567+AnnaiaBokungu@users.noreply.github.com>
2022-09-28 23:51:15 -0400

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -3,3 +3,6 @@ MechaCar_dataframe <- read.csv(file.choose())
 lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data = MechaCar_dataframe)
 summary(lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data = MechaCar_dataframe))
 
+suspension_coils <- read.csv(file.choose())
+total_summary <- suspension_coils %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+lot_summary <- suspension_coils %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
